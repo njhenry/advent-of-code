9f0570a81305c86b47605c7f1c32729705ecfad2
Nathaniel Henry <nat.henry7@gmail.com>
2023-12-09 13:39:25 -0800

    Solutions for days 8 and 9.

--- 2023/day_09.R
+++ 2023/day_09.R
@@ -1,6 +1,6 @@
 ## #######################################################################################
 ##
-## ADVENT OF CODE DAY OCHO
+## ADVENT OF CODE DAY 09
 ##
 ## AUTHOR: Nat Henry, nat@henryspatialanalysis.com
 ## DATE: 2023-12-07 (ET)
@@ -35,108 +35,28 @@ load_input_as_text <- function(day, year = 2023, cache_dir = Sys.getenv('AOC_CAC
   return(input)
 }
 
-split_on_val <- function(char_vec, split_val){
-  list_idx <- cumsum(char_vec == split_val) + 1
-  out_list <- lapply(
-    seq(max(list_idx)),
-    function(ii) char_vec[list_idx == ii] |> setdiff(y = "")
-  )
-  # If the input ends with the split line (e.g. a blank line), drop it
-  if(length(out_list[[length(out_list)]]) == 0) out_list[[length(out_list)]] <- NULL
-  return(out_list)
-}
+input_list <- load_input_as_text(day = 9)
 
-input_list <- load_input_as_text(day = 8) |> split_on_val(split_val = "")
-
-# L => 1, R => 2
-rl <- input_list[[1]] |> strsplit(split = '') |> unlist() |> 
-  sapply(function(x) if(x == 'L') 1 else 2)
-
-# Parse network as named list
-map_list <- vector('list', length = length(input_list[[2]]))
-headers <- vector('character', length = length(input_list[[2]]))
-for(ii in seq_along(map_list)){
-  this_item_text <- input_list[[2]][ii]
-  headers[ii] <- substr(this_item_text, 1, 3)
-  map_list[[ii]] <- c(substr(this_item_text, 8, 10), substr(this_item_text, 13, 15))  
-}
-names(map_list) <- headers
-
-# Some helper functions
-get_step_remainder <- function(step_counts, size = length(rl)){
-  remainders <- step_counts %% size
-  remainders[remainders == 0 ] <- size
-  return(remainders)
-}
-map_apply_step <- function(locs, next_step, the_map = map_list){
-  Map(f = function(x) x[next_step], the_map[locs]) |> unlist()
-}
-
-# Part 1 solution
-current_loc <- 'AAA'
-end_loc <- 'ZZZ'
-step_count <- 0
-
-while(current_loc != end_loc){
-  step_count <- step_count + 1
-  # Repeat list items if necessary
-  step_remainder <- get_step_remainder(step_count)
-  next_step <- rl[step_remainder]
-  current_loc <- map_apply_step(current_loc, next_step)
-}
-message(step_count)
-
-# Part 2 solutions
-start_locs <- grep("A$", headers, value = T)
-possible_solutions <- grep("Z$", headers, value = T)
-check_end_condition <- function(locs) all(locs %in% possible_solutions)
-
-solutions_list <- lapply(start_locs, function(x){
-  y <- vector('list', length = length(possible_solutions))
-  names(y) <- possible_solutions
-  return(y)
-})
-names(solutions_list) <- start_locs
-
-check_period_condition <- function(step_counts){
-  max(table(get_step_remainder(step_counts))) > 1
-}
-
-# Check solutions for each start point separately and find the map period
-for(start_loc in start_locs){
-  message("Finding period starting at ", start_loc)
-  current_loc <- start_loc
-  step_count <- 0
-  period_condition_met <- FALSE
-  while(!period_condition_met){
-    step_count <- step_count + 1
-    step_remainder <- get_step_remainder(step_count)
-    next_step <- rl[step_remainder]
-    current_loc <- map_apply_step(current_loc, next_step)
-    end_condition_met <- check_end_condition(current_loc)
-    if(end_condition_met){
-      # Append the latest step count that matches the starting position to this solution
-      solutions_list[[start_loc]][[current_loc]] <- c(
-        solutions_list[[start_loc]][[current_loc]],
-        step_count
-      )
-      # If the step remainder for this solution is the same as for any previous step
-      #  count, we have found the period!
-      period_condition_met <- check_period_condition(
-        solutions_list[[start_loc]][[current_loc]]
-      )
-    }
+get_next_val <- function(int_vec){
+  diff_vec <- diff(int_vec)
+  if(uniqueN(diff_vec) == 1){
+    return(last(int_vec) + last(diff_vec))
+  } else {
+    return(last(int_vec) + get_next_val(diff_vec))
   }
-  message("Period for ", start_loc, " found after ", step_count, " steps.")
 }
 
-# Coincidentally, all of the starting points have periods that are multiples of the
-#  step list
-periods <- sapply(start_locs, function(sl) min(unlist(solutions_list[[sl]])))
-if(any(get_step_remainder(periods) != length(rl))){
-  stop("Periods not the length of the instructions list")
+get_previous_val <- function(int_vec){
+  diff_vec <- diff(int_vec)
+  if(uniqueN(diff_vec) == 1){
+    return(first(int_vec) - first(diff_vec))
+  } else {
+    return(first(int_vec) - get_previous_val(diff_vec))
+  }
 }
 
-# Simply find the least common multiple of all periods
-p2_solution <- DescTools::LCM(periods)
-message(p2_solution)
+int_list <- strsplit(input_list, split = ' ') |> lapply(as.integer)
+p1_answer <- lapply(int_list, get_next_val) |> unlist() |> sum()
+message(p1_answer)
+p2_answer <- lapply(int_list, get_previous_val) |> unlist() |> sum()
+message(p2_answer)
